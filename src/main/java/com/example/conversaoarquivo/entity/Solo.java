e3c9150b20826d18014f28b3f024671c1da67fed
alessandraesteph <alessandraestph@gmail.com>
2025-02-14 02:35:19 -0300

    Commit inicial

--- src/main/java/com/example/conversaoarquivo/entity/Solo.java
+++ src/main/java/com/example/conversaoarquivo/entity/Solo.java
@@ -8,11 +8,18 @@ public class Solo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
+
     public String X;
+
     public String Y;
+
     public String Materia_Organica;
+
     public String Potassio;
+
     public String Fosforo;
+
     public String Nitrogenio;
+
     public String Magnesio;
 }
